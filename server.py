914d856d64f1cdbceb3cbe2b553bbd4aa4d97448
unknown <mateusz.rac@gmail.com>
2023-10-31 09:21:16 +0100

    Code

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ for device_adress in onewire_devices:
         if match:
             
             T = float(match.group(1))/1000
-            #print(device_adress+'\t'+str(T))
+            print(device_adress+'\t'+str(T))
 
             current_time = datetime.utcnow()
             timestamp = current_time.strftime("%Y-%m-%dT%H:%M:%SZ")
