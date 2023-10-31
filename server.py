f086cfe58af8af796775c9970fe493b8ce6127e1
unknown <mateusz.rac@gmail.com>
2023-10-31 08:04:39 +0100

    Code

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import re
 onewiredir = '/sys/bus/w1/devices/'
 onewire_devices = os.listdir(onewiredir)
 
-print(onewire_devices)
 
 for device_adress in onewire_devices:
     if device_adress[:2]!='00' and device_adress[:2]!='w1':
@@ -14,6 +13,12 @@ for device_adress in onewire_devices:
             file_content = file.read()
 
         pattern = r't=(\d+)'
-        matches = re.findall(pattern, file_content)
-        T = [float(match) for match in matches]/1000
-        print(device_adress+'\t'+str(T))
+        match = re.search(pattern, file_content)
+        
+        if match:
+            
+            T = float(match.group(1))/1000
+            print(device_adress+'\t'+str(T))
+
+        else:
+            print("No match found")
