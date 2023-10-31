a8ac0c7846e07bbadbaec656ed123cdd532e3aae
unknown <mateusz.rac@gmail.com>
2023-10-31 10:36:11 +0100

    Code

--- server.py
+++ server.py
@@ -6,6 +6,39 @@ import board
 import requests
 
 
+
+
+# Replace these values with your own database credentials
+db_config = {
+    "host": "poprunthyb216.mysql.db",
+    "user": "poprunthyb216",
+    "password": "your_password",
+    "database": "poprunthyb216"
+}
+
+def upload_to_db(db_config,variable,timestamp,value):
+    
+    conn = mysql.connector.connect(**db_config)
+    
+    # Create a cursor object to interact with the database
+    cursor = conn.cursor()
+
+    # Construct the SQL query to insert data into a specific table
+    table_name = "rpi"
+    columns = "variable, timestamp, value"
+    values = "'"+variable+"', '"+timestamp+"', '"+str(value)+"'"
+    insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({variable})"
+
+    # Execute the query with the data
+    cursor.execute(insert_query)
+
+    # Commit the changes to the database
+    conn.commit()
+
+    # Close the cursor and the database connection
+    cursor.close()
+    conn.close()
+
 onewiredir = '/sys/bus/w1/devices/'
 onewire_devices = os.listdir(onewiredir)
 
@@ -36,6 +69,7 @@ for device_adress in onewire_devices:
 
                 if T > -50 and T<100:
                     data_array.append({"variable": device_adress, "points":[[timestamp,T]]})
+                    upload_to_db(db_config,device_adress,timestamp,T)
             else:
                 print("No match found")
     except:
@@ -75,3 +109,29 @@ else:
     
     
 print(data_array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
