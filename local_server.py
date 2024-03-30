3b2997905c6b2a572567d11478f8a32e8598a544
unknown <mateusz.rac@gmail.com>
2024-03-30 21:11:21 +0100

    DB test

--- local_server.py
+++ local_server.py
@@ -6,8 +6,8 @@ from datetime import datetime
 engine = create_engine('mysql+pymysql://pi:raspberry@localhost:3306/db')
 
 df = pd.DataFrame(columns=['variable','timestamp','value'])
-df.loc[len(df)] = ['TEST',datetime.datetime(2024,3,30,12,0),69]
-df.loc[len(df)] = ['TEST',datetime.datetime(2024,3,30,12,0),666]
+df.loc[len(df)] = ['TEST',datetime(2024,3,30,12,0),69]
+df.loc[len(df)] = ['TEST',datetime(2024,3,30,12,0),666]
 
 # Replace 'meteo' with your table name
 df.to_sql('meteo', con=engine, if_exists='append', index=False)
